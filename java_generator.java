6d0f2d25270e282555c87f8ea697c2c58fb5d6ae
SeredovaSophia <162325967+SeredovaSophia@users.noreply.github.com>
2025-05-12 05:16:38 +0400

    Генератор Java

---
+++ java_generator.java
@@ -0,0 +1,25 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class RandomBinarySequence {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int length = 128;
+        StringBuilder binarySequence = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            binarySequence.append(random.nextInt(2));
+        }
+
+        System.out.println("Псевдослучайная бинарная последовательность (128 бит): " + binarySequence);
+
+        try (FileWriter fileWriter = new FileWriter("java_bin_seq.txt")) {
+            fileWriter.write(binarySequence.toString());
+            System.out.println("Последовательность сохранена в java_bin_seq.txt.");
+        } catch (IOException e) {
+            System.out.println("Ошибка: Не удалось открыть файл для записи.");
+            e.printStackTrace();
+        }
+    }
+}
